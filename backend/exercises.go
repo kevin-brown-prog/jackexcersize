716db78b1456e95deeebdd866ca81d5fd06ef4a5
libdata@gmail.com <libdata@gmail.com>
2022-07-07 19:51:16 -0400

    Try to get it to work

---
+++ backend/exercises.go
@@ -0,0 +1,93 @@
+package backend
+
+import (
+	"time"
+)
+
+/*
+var set1 = { set_id : 0,
+	weight : 135,
+	reps_or_duration : 5,
+	done : false,
+	is_time_based: false
+	};
+
+	var set2 = { set_id : 1,
+	  weight : 135,
+	  reps_or_duration : 5,
+	  done : false,
+	  is_time_based: false,
+	 };
+
+	 var set3 = { set_id : 2,
+	  weight : 135,
+	  reps_or_duration : 5,
+	  done : false,
+	  is_time_based: false,
+	  };
+
+	  var Exercise1 = {
+		name:"Bench Press",
+		sets :[set1, set2,set3]
+
+	  }
+
+
+	   set1 = { set_id : 3,
+		weight : 135,
+		reps_or_duration : 5,
+		done : false,
+		is_time_based: false
+		};
+
+		 set2 = { set_id : 4,
+		  weight : 135,
+		  reps_or_duration : 5,
+		  done : false,
+		  is_time_based: false,
+		 };
+
+		  set3 = { set_id : 5,
+		  weight : 135,
+		  reps_or_duration : 5,
+		  done : false,
+		  is_time_based: false,
+		  };
+
+
+
+	  var Exercise2 = {
+		name:"Squats",
+		sets:[set1, set2, set3]
+	  }
+
+	  let ExerciseSession ={
+		name:"Monday session",
+		date:new Date(),
+		exercises : [Exercise1, Exercise2]
+	  }
+
+
+weight : 135,
+	  reps_or_duration : 5,
+	  done : false,
+	  is_time_based: false,
+
+*/
+type Set struct {
+	Weight             int       `firestore:"weight" json:"weight"`
+	RepsOrDuration     int       `firestore:"reps_or_duration" json:"reps_or_duration"`
+	Done               bool      `firestore:"done" json:"done"`
+	IsTimeBased        bool      `firestore:"is_time_based" json:"is_time_based"`
+	TimeStampAdded     time.Time `firestore:"TimeStampAdded" json:"TimeStampAdded"`
+	TimeStampCompleted time.Time `firestore:"TimeStampCompleted" json:"TimeStampCompleted"`
+}
+type Excercise struct {
+	Name string `firestore:"name" json:"name"`
+	Sets Set    `firestore:"sets" json:"sets"`
+}
+type ExcerciseSession struct {
+	Name       string    `firestore:"name" json:"name"`
+	Date       time.Time `firestore:"date" json:"date"`
+	Excercises Excercise `firestore:"exercises" json:"excercises"`
+}
--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,15 @@
-package main
+package backend
 
 //https://www.digitalocean.com/community/tutorials/debugging-go-code-with-visual-studio-code
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"time"
+
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
 )
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
@@ -11,7 +17,27 @@ func HelloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	fmt.Println("Hello, world!")
+	fmt.Println("Hello, world")
+
+	ctx := context.Background()
+	sa := option.WithCredentialsFile("jackworkout.json")
+	app, err := firebase.NewApp(ctx, nil, sa)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	firestore, err := app.Firestore(ctx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer firestore.Close()
+
+	session := firestore.Collection("ExcerciseSessions").NewDoc()
+	session.Set(ctx, Set{Weight: 135, RepsOrDuration: 10, Done: false, IsTimeBased: false, TimeStampAdded: time.Now()})
+	session = firestore.Collection("ExcerciseSessions").NewDoc()
+	session.Set(ctx, Set{Weight: 135, RepsOrDuration: 10, Done: false, IsTimeBased: false, TimeStampAdded: time.Now()})
+	session = firestore.Collection("ExcerciseSessions").NewDoc()
+	session.Set(ctx, Set{Weight: 135, RepsOrDuration: 10, Done: false, IsTimeBased: false, TimeStampAdded: time.Now()})
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/hello", HelloHandler)
 
